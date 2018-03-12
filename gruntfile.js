893ff04a1f681d3b89dfab16e3e4b34aba476cd3
Louis Thomas Kelly <845862616@bun2-206-18.local>
2018-03-12 14:16:26 +1300

    added automation functionality and scss compiler

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,62 @@
-// gruntfile.js
 module.exports = function(grunt){
 	grunt.initConfig({
+		jshint: {
+			files: ["*.js", "js/scripts.js"],
+			options: {
+				globals:{
+					jQuery:true
+				}
+			}
+		},
+		csslint: {
+			strict: {
+				options: {
+					import:2
+				},
+				src: ['css/styles.css']
+
+			}
+		},
+		uglify: {
+    		my_target: {
+		      files: {'FINAL/js/scripts.min.js': ['js/scripts.js']}
+	    	}
+	  	},
+	  	cssmin: {
+		  target: {
+		    files: [{
+		      expand: true,
+		      // cwd: 'css',
+		      src: ['css/*.css', '!*.min.css'],
+		      dest: 'FINAL',
+		      ext: '.min.css'
+		    }]
+		  }
+		},
+		watch: {
+			scss:{
+				files:['sass/styles.scss'],
+				tasks:['sass','cssmin']
+			},
+			js:{
+				files:['js/scripts.js'],
+				tasks:['jshint','uglify']
+			},
+			html:{
+				files:['index.html'],
+				tasks:['htmlmin']
+			}
+		},
+		sass: {                              // Task
+		    dist: {                            // Target
+		      options: {                       // Target options
+		        style: 'expanded'
+		      },
+		      files: {                         // Dictionary of files
+		        'css/styles.css': 'sass/styles.scss'
+		      }
+		    }
+		  },
 		htmlmin: {
 			dist: { 
       			options: {
@@ -8,7 +64,7 @@ module.exports = function(grunt){
 			        collapseWhitespace: true
 			    },
 			    files: {
-        			'HTML/index.html': 'index.html'
+        			'FINAL/index.html': 'index.html'
         		}
       		}
 		}
@@ -16,20 +72,19 @@ module.exports = function(grunt){
 
 	});
 
-	// grunt.loadNpmTasks('grunt-contrib-jshint');
-	// grunt.loadNpmTasks('grunt-contrib-csslint');
-	// grunt.loadNpmTasks('grunt-contrib-uglify');
-	// grunt.loadNpmTasks('grunt-contrib-cssmin');
-	// grunt.loadNpmTasks('grunt-contrib-watch');
-	// grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-	grunt.registerTask("default", ["htmlmin"]);
-	// grunt.registerTask("debug", ["csslint"]);
-	// grunt.registerTask("js", ["jshint","uglify"]);
-	// grunt.registerTask("sassy", ["sass"]);
-	// grunt.registerTask("checkcss", ["csslint"]);
-	// grunt.registerTask("html", ["htmlmin"]);
-	// grunt.registerTask("watchy",['watch']);
-	// grunt.registerTask("dothing",["htmlmin"]);
+	grunt.registerTask("default", ["jshint","csslint"]);
+	grunt.registerTask("debug", ["csslint"]);
+	grunt.registerTask("js", ["jshint","uglify"]);
+	grunt.registerTask("sassy", ["sass"]);
+	grunt.registerTask("checkcss", ["csslint"]);
+	grunt.registerTask("html", ["htmlmin"]);
+	grunt.registerTask("watchy",['watch']);
 };
