accea3a163addf2dbc16805b4089285fd3b2cb40
Louis Thomas Kelly <845862616@bun2-206-18.local>
2018-03-13 15:22:22 +1300

    added map model and sidebar, added google map, added jQuery, added horozontal flip to sidebar via CSS and JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-// alert('yo');
+//Louis Code
+$("#map-modal").modal({
+   fadeDuration: 200,
+   width: 4000
+});
+
+function initMap(){
+	var uluru = {lat: -25.363, lng: 131.044};
+	var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+}
+
+$('.testBtn').click(function(){
+	$( ".flip-container" ).toggleClass( "flip" );
+});
